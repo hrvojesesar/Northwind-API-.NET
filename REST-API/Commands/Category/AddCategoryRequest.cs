b53534a70fd65027cc587d7a81c7d9905c520ba7
hrvojesesar007 <hrvojesesar007@gmail.com>
2024-09-28 16:33:20 +0200

    Minor fix

--- REST-API/Commands/Category/AddCategoryRequest.cs
+++ REST-API/Commands/Category/AddCategoryRequest.cs
@@ -5,8 +5,8 @@ namespace REST_API.Commands.Category;
 public class AddCategoryRequest
 {
     [Required]
-    public string CategoryName { get; set; }
     [MaxLength(15)]
+    public string CategoryName { get; set; }
     public string? Description { get; set; }
     public byte[]? Picture { get; set; } = null!;
 }
--- REST-API/Commands/Category/EditCategoryRequest.cs
+++ REST-API/Commands/Category/EditCategoryRequest.cs
@@ -1,8 +1,11 @@
-﻿namespace REST_API.Commands.Category;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace REST_API.Commands.Category;
 
 public class EditCategoryRequest
 {
     public int CategoryID { get; set; }
+    [MaxLength(15)]
     public string CategoryName { get; set; }
     public string? Description { get; set; }
     public byte[]? Picture { get; set; }
