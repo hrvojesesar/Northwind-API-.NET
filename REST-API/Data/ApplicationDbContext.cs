96a6643626670165cc14ab88a601ec05bb21c522
hrvojesesar007 <hrvojesesar007@gmail.com>
2024-09-28 14:12:41 +0200

    Setup Database

---
+++ REST-API/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace REST_API.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    
+}
